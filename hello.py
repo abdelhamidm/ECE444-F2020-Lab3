7490b891e96fbb3874f9f253d91321b815e44e4e
Mohamed Abdelhamid <abdelhamidm@MacBook-Pro-7.local>
2020-09-30 11:49:27 -0400

    Commiting web form changes and screenshots

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -14,6 +15,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[Email()])
     submit = SubmitField('Submit')
 
 
@@ -31,12 +33,22 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+
+        #Check if name changed
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+
+        #Check if email changed
+        old_email = session.get('email')
+        if old_email is not None and old_name != form.email.data:
+            flash('Looks like you have changed your email!')
+        session['email'] = form.email.data
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+       
 
 #run the server to service web requests
 if __name__ == '__main__':
